b8486d936c583dfbec5e123e2d6a10742c4bf202
Gabriela <gabriela.adascalitei1@gamil.com>
2025-02-25 22:10:26 +0200

    numar intreg

--- incercare.c
+++ incercare.c
@@ -2,5 +2,9 @@
 int main()
 {
 	printf("Afisare\n");
+	int var = 0;
+	printf("Gandeste te la un numar. Il voi ghici: ");
+	scanf_s("%d", &var);
+	printf("\n\nTe-ai gandit la numarul: %d", var);
 	return 0;
 }
