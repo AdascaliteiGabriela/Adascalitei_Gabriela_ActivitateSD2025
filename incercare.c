4e51d8860b53673bc0d046fec7d2c5042add7201
Gabriela <gabriela.adascalitei1@gamil.com>
2025-02-26 16:00:19 +0200

    modificare numar locuri

--- incercare.c
+++ incercare.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-int main()
-{
-	printf("Afisare\n");
-	int var = 0;
-	printf("Gandeste te la un numar. Il voi ghici: ");
-	scanf_s("%d", &var);
-	printf("\n\nTe-ai gandit la numarul: %d", var);
-	//blabla
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Afisare\n");
+//	int var = 0;
+//	printf("Gandeste te la un numar. Il voi ghici: ");
+//	scanf_s("%d", &var);
+//	printf("\n\nTe-ai gandit la numarul: %d", var);
+//	//blabla
+//	return 0;
+//}
