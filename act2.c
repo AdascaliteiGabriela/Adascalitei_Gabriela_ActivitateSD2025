9a648efde411a771822d8df8877e20191f2fd0d3
Gabriela <gabriela.adascalitei1@gamil.com>
2025-03-11 20:00:50 +0200

    adaugat vector de Salariati + afisare

--- act2.c
+++ act2.c
@@ -45,11 +45,18 @@ void dezalocare(struct Salariat* s)
 			s->nume = NULL;
 	}
 }
+void afisareVector(struct Salariat* vector, int nrElemente) {
+	for (int i = 0; i < nrElemente; i++)
+		afisare(vector[i]);
+	printf("\n");
+}
+
+
 
 int main()
 {
 	struct Salariat s1;
-	s1=initializare(3, 2100, "Gigel", 's');
+	s1=initializare(3, 2100, "Gigel", 'S');
 	struct Salariat s2;
 	s2.id = 2;
 	s2.salariu = 3210.5;
@@ -58,5 +65,23 @@ int main()
 	strcpy_s(s2.nume,strlen("Maria")+1, "Maria");
 	afisare(s1);
 	afisare(s2);
+	printf("\n");
+	struct Salariat s3;
+	s3 = initializare(1, 3456.2, "Patricia", 'L');
+	struct Salariat* vector;
+	vector = (struct Salariat*)malloc(3 * sizeof(struct Salariat));
+	int numarElemente = 3;
+	//vector[0] = s3;
+	//vector[1] = s2;     - shallow copy
+	//vector[2] = s1;
+	//afisareVector(vector, numarElemente);
+	///*dezalocare(&s1);
+	//afisareVector(vector, numarElemente);*/
+	vector[2] = initializare(s1.id, s1.salariu, s1.nume, s1.specializare);
+	vector[1] = initializare(s2.id, s2.salariu, s2.nume, s2.specializare);
+	vector[0] = initializare(s3.id, s3.salariu, s3.nume, s3.specializare);
+	afisareVector(vector, numarElemente);
+	dezalocare(&s1);
+	afisareVector(vector, numarElemente);
 	return 0;
 }
