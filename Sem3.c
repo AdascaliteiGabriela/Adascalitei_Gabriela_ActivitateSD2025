e0388dc619d41813bf44c8506c262a4afafe2f5d
Gabriela <gabriela.adascalitei1@gamil.com>
2025-03-12 15:54:27 +0200

    Adaugat functie de citire masina

--- Sem3.c
+++ Sem3.c
@@ -17,7 +17,7 @@ struct StructuraMasina {
 typedef struct StructuraMasina Masina; //in loc sa scriu struct StructuraMasina am definit un alias Masina 
 
 void afisareMasina(Masina masina) {
-	printf("ID masina: %d\nNumar usi: %d \nPretul: %5.2f \nModelul: %cs \nNume sofer: %s \nSerie: %c\n\n", masina.id, masina.nrUsi, masina.pret, masina.model, masina.numeSofer, masina.serie);
+	printf("ID masina: %d\nNumar usi: %d \nPretul: %5.2f \nModelul: %s \nNume sofer: %s \nSerie: %c\n\n", masina.id, masina.nrUsi, masina.pret, masina.model, masina.numeSofer, masina.serie);
 }
 
 void afisareVectorMasini(Masina* masini, int nrMasini) {
@@ -27,13 +27,40 @@ void afisareVectorMasini(Masina* masini, int nrMasini) {
 }
 
 void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
-	//adauga in vectorul primit o noua masina pe care o primim ca parametru
-	//ATENTIE - se modifica numarul de masini din vector;
+	Masina* copie = (Masina*)malloc(((*nrMasini) + 1) * sizeof(Masina));
+	for (int i = 0; i < (*nrMasini); i++)
+	{
+		copie[i] = (*masini)[i];//fac shallow copy si nu modific pointerii ca sa fie legaturile bune sau daca fac deep copy, trebuie intai sterse inainte de realocare
+	}
+	copie[(*nrMasini)] = masinaNoua;
+	free((*masini));//free e o functie
+	(*masini) = copie;//am mutat pointer ul la adresa lui copie
+
 }
 
 Masina citireMasinaFisier(FILE* file) {
-	//functia citeste o masina dintr-un strceam deja deschis
-	//masina citita este returnata;
+	char buffer[100];
+	char sep[3] = ",\n";
+	char* aux;
+	fgets(buffer, 100, file);
+	Masina m1;
+	aux= strtok(buffer, sep);
+	m1.id = atoi(aux);//cast din codul ASCII la int prin functia atoi()
+	m1.nrUsi = atoi(strtok(NULL, sep));//ca sa se uite/sa continue cautarea de unde a ramas 
+	m1.pret = atof(strtok(NULL, sep));//atof e importat doar in stdlib
+	//trb citit intr un buffer, apoi alocam spatiu pentru char* si copiem, altfel s ar pierde valorile cum tot ce citim aici e salvat local in stack-ul functiei si nu in heap
+	aux = strtok(NULL, sep);
+	m1.model = (char*)malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+	aux = strtok(NULL, sep);
+	m1.numeSofer = (char*)malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+	m1.serie = strtok(NULL, sep)[0];//dereferentiere sau indexare de 0 ca sa luam doar un caracter(primul)
+	return m1;
+
+
+
+
 }
 
 Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
@@ -49,7 +76,6 @@ void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
 
 int main() 
 {
-
-
+	//in masini.txt despartitorul e virgula pentru a putea citi denumiri cu spatiu
 	return 0;
 }
