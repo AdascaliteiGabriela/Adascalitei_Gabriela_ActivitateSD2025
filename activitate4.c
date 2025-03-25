5caa9bb906c651b5d033dcf35f5d97352ca12dd7
Gabriela <gabriela.adascalitei1@gamil.com>
2025-03-25 21:56:33 +0200

    adaugat functie de adaugare in vector si citire vector din fisier

--- activitate4.c
+++ activitate4.c
@@ -26,25 +26,20 @@ struct Caiet initializareCaiet(int numarPagini, const char* disciplina, char tip
 	}
 	return c;
 }
-struct Caiet citireCaietFisier(char* numef, struct Caiet* c)
+struct Caiet citireCaietFisier(FILE*f)
 {
-	FILE* f = fopen(numef, "r");
-	
-	
-
-		char buffer[100];
-		fgets(buffer, 100, f);
-		char separatori[3] = "/\n";
+	struct Caiet c;
+	char buffer[100];
+	fgets(buffer, 100, f);
+	char separatori[3] = "/\n";
+	c.numarPagini = atoi(strtok(buffer, separatori));
+	char* aux;
+	aux = strtok(NULL, separatori);
+	c.disciplina = (char*)malloc(strlen(aux) + 1);
+	strcpy_s(c.disciplina, strlen(aux) + 1, aux);
+	c.tip = strtok(NULL, separatori)[0];
 		
-		
-		(*c).numarPagini = atoi(strtok(buffer, separatori));
-		char* aux;
-		aux = strtok(NULL, separatori);
-		(*c).disciplina = (char*)malloc(strlen(aux) + 1);
-		strcpy_s((*c).disciplina, strlen(aux) + 1, aux);
-		(*c).tip = strtok(NULL, separatori)[0];
-		
-	
+	return c;
 
 }
 void afisareCaiet(struct Caiet c)
@@ -58,11 +53,43 @@ void dezalocareCaiet(struct Caiet* c)
 		//echivalent cu free(c->disciplina);
 	
 }
+void caietInVector(struct Caiet** caiete, int* numarCaiete, struct Caiet caietNou)
+{
+	struct Caiet* aux=(struct Caiet*)malloc((*numarCaiete+1)*sizeof(struct Caiet));
+	for (int i = 0; i < *numarCaiete; i++)
+		aux[i] = (*caiete)[i];
+	aux[*numarCaiete] = caietNou;
+	free(*caiete);
+	(*caiete) = aux;
+	(*numarCaiete)++;
+}
+struct Caiet* citireVectorFisier(int* numar, FILE* f)
+{
+	struct Caiet* caiete = NULL;
+	(*numar) = 0;
+	while (!feof(f))
+	{
+		struct Caiet c1 = citireCaietFisier(f);
+		caietInVector(&caiete, &(*numar), c1);
+	}
+	return caiete;
+
+}
+void afisareVector(struct Caiet* c, int numar)
+{
+	for (int i = 0; i < numar; i++)
+		afisareCaiet(c[i]);
+}
 int main()
 {
 	struct Caiet c1;
-	citireCaietFisier("caiete.txt", &c1);
+	FILE* f = fopen("caiete.txt", "r");
+	c1=citireCaietFisier(f);
 	afisareCaiet(c1);
 	dezalocareCaiet(&c1);
+	struct Caiete* c = NULL;
+	int numar = 0;
+	c=citireVectorFisier(&numar, f);
+	afisareVector(c, numar);
 	return 0;
 }
